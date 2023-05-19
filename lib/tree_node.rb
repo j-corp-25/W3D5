8ce1149ca5ccc77b568bed611748f8bf4692dc7f
Jordy <jordycorporan95@gmail.com>
2023-05-19 14:00:37 -0400

    Initialized parent

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,5 +1,31 @@
 class PolyTreeNode
+  attr_reader "value","children"
   def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+
+
   end
+
+  def parent
+   self.parent
+  end
+
+  def children
+    self.children
+  end
+
+  
+
+  # def value
+  #   self.value
+  # end
+
+  # # def parent=(nil)
+  #   if parent != nil
+
+
+
 
 end
